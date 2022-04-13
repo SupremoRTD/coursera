2bacc66be83e3dd11412d99bff070d679850f551
Steve Saldivar <steve.saldivar@gmail.com>
2022-04-13 05:38:23 -0700

    Lecture 59

---
+++ module5-site/js/script.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('#navbarToggle').blur(function (event) {
+    const screenWidth = window.innerWidth
+    if (screenWidth < 768) {
+      $('#collapsable-nav').collapse('hide')
+    }
+  })
+})
+;(function ($window) {
+  const DC = {}
+  const HOME_HTML = 'snippets/home-snippet.html'
+
+  const insertHtml = function (selector, html) {
+    const targetElem = document.querySelector(selector)
+    targetElem.innerHTML = html
+  }
+
+  const showLoading = function (selector) {
+    let html = '<div class="text-center">'
+    html += '<img src="images/ajax-loader.gif"></div>'
+    insertHtml(selector, html)
+  }
+})(window)
